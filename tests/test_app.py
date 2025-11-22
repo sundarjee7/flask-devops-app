693567f404fdcf8b9c4cfb1c53b1ab906b359934
Sundarjee R <suraine36@gmail.com>
2025-11-22 13:37:52 +0000

    Updated login page and tests, Jenkins pipeline working

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,12 @@
-import sys
-sys.path.append('/app')  
-	
 from app import app
 
-def test_home():
+def test_home_status_code():
+    # Test if the login page loads successfully
     response = app.test_client().get('/')
     assert response.status_code == 200
+
+def test_home_content():
+    # Test if the login page contains "Login" text
+    response = app.test_client().get('/')
+    assert b"Login" in response.data
 

fca2e536101f33fc98506921b294f8f24a8f5b1d
Sundarjee R <suraine36@gmail.com>
2025-11-22 12:19:22 +0000

    Added test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,6 @@
-# test_app.py
 from app import app
 
 def test_home():
-    client = app.test_client()
-    response = client.get('/')
+    response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Welcome to My Flask App Home Page" in response.data
 
